04f861e7c3847dadb82b0cfa05cd1a2a5e8dcdfe
Vitor Domith <vcdomith@hotmail.com>
2023-12-15 17:59:47 -0300

    SVGR bug

--- src/components/Rodape/Rodape.tsx
+++ src/components/Rodape/Rodape.tsx
@@ -1,11 +1,11 @@
 import styles from './Rodape.module.css'
-import { ReactComponent as MarcaRegistrada } from '../../assets/marca_registrada.svg'
+// import { ReactComponent as MarcaRegistrada } from '../../assets/marca_registrada.svg'
 
 
 const Rodape = () => {
   return (
     <footer className={styles.rodape}>
-        <MarcaRegistrada />
+        {/* <MarcaRegistrada /> */}
 
         Desenvolvido por Vitor
     </footer>
